f48d6267665096a3927b9064b89fd59ea0be98a0
Alex C <alex.m.calia@outlook.com>
2024-01-28 15:38:32 -0500

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import express from 'express';
+import http from 'http';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import compression from 'compression';
+
+const app = express();
+
+app.use(cors());
+
+app.use(compression());
+app.use(cookieParser());
+app.use(bodyParser.json());
+
+const server = http.createServer(app);
+
+server.listen(8080, () => {
+  console.log("Server running on http://localhost:8080")
+});
